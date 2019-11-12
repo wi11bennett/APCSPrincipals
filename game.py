b75787ef167964fb6e6c88191f311b0a879927d3
wi11bennett <57371198+wi11bennett@users.noreply.github.com>
2019-11-12 12:32:09 -0800

    Add files via upload

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import random
 rnum = random.randint (1000, 9999)
-#guesses vriable
+#guesses variable
 guesses = 5
 
 strnum = str (rnum)
@@ -10,10 +10,6 @@ for digit in strnum:
     rnumlist.append (int(digit))
 
 usersuccess = [ "-","-","-","-"]
-print (usersuccess)
-if indexguess == indexrando:
-    replace
-
 
 while guesses != 0:
     userinput1 = input ("Enter 4 numbers: ")
@@ -28,7 +24,8 @@ while guesses != 0:
         for i in rnumlist:
             for j in guesslist:
                 if i == j :
-                    print (rnumlist.index(i))
+                    if rnumlist.index(i) == guesslist.index(j)
+                    print (usersuccess)
                 else:
                      print ("wrong")
 
