871a0e8c5fb77b8af6b53f45a3b2e420e1d00ea4
wi11bennett <57371198+wi11bennett@users.noreply.github.com>
2019-11-21 12:30:43 -0800

    Add files via upload

--- game.py
+++ game.py
@@ -1,32 +1,79 @@
+#selects random number that u guess
 import random
 rnum = random.randint (1000, 9999)
-#guesses variable
+print (rnum)
+#guesses vriable
 guesses = 5
+usersuccess = ["-", "-", "-", "-"]
 
+#makes random nmber list so that it can be compared to the number that is guessed
 strnum = str (rnum)
 rnumlist = []
 guesslist = []
 for digit in strnum:
     rnumlist.append (int(digit))
+print (rnumlist)
 
-usersuccess = [ "-","-","-","-"]
-
+#user input
 while guesses != 0:
     userinput1 = input ("Enter 4 numbers: ")
 
+#checks if everything is correct, says congrats if it is
     for digit in userinput1 :
         guesslist.append(int(digit))
     if guesslist == rnumlist:
         print("Congrats")
-        guesses = 0
-
+        userinput2 = input("Do you want to play again?")
+        if userinput2.lower() == ("yes"):
+            rnumlist = []
+            guesslist = []
+            rnum = random.randint (1000, 9999)
+            guesses = 5
+        else:
+            print("thanks for playing")
+#if it isnt right then it checks each index to see if they match and shows the player what numbrs match
     else:
-        for i in rnumlist:
-            for j in guesslist:
-                if i == j :
-                    if rnumlist.index(i) == guesslist.index(j)
-                    print (usersuccess)
-                else:
-                     print ("wrong")
+        for i in guesslist:
+            if i in rnumlist:
+                indexnumber = 0
+                for j in rnumlist:
+                    if j == i:
+                        usersuccess.pop(indexnumber)
+                        usersuccess.insert(indexnumber, i)
+                    indexnumber += 1
 
-    guesslist.clear()
+        guesses -= 1
+        if guesses == 0:
+            print ("out of guesses")
+            userinput2 = input ("Do you want to play again?")
+            if userinput2.lower() == ("yes") :
+                guesses = 5
+            else:
+                print ("thanks for playing")
+
+
+        else:
+            print("Sorry. You have " + str(guesses) + "  guesses left.")
+
+
+        print(usersuccess)
+
+    #     for i in guesslist:
+    #         for j in rnumlist:
+    #             if i == j :
+    #                 rnumindex = rnumlist.index(j)
+    #                 guessindex = guesslist.index(i)
+    #                 if rnumindex == guessindex:
+    #
+    #                     guesses += 1
+    #
+    #                     rnumlist.pop(rnumindex)
+    #                     rnumlist.insert(rnumindex, "-")
+    # guesses -= 1
+    # print (guesses, usersuccess)
+
+
+    guesslist.clear ()
+
+if guesses == 0:
+    print(rnum)
